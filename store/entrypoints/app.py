5fec1720ca536ff2da713d1e788a8a8e856f30ca
Bogdan Objilean <bodea0001@gmail.com>
2025-03-06 22:45:01 +0300

    feat(app): add exception handlers

--- store/entrypoints/app.py
+++ store/entrypoints/app.py
@@ -3,6 +3,7 @@ from litestar import Litestar, Router
 from controllers import controllers
 
 from handlers.auth import jwt_auth
+from handlers.exceptions import exception_handlers
 
 
 api_router = Router(path="/api", route_handlers=controllers)
@@ -10,4 +11,5 @@ api_router = Router(path="/api", route_handlers=controllers)
 app = Litestar(
     route_handlers=[api_router],
     on_app_init=[jwt_auth.on_app_init],
+    exception_handlers=exception_handlers,
 )
